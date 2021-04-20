c276d0998d2497476273a39d6e8f86f1b39c63cc
shirayukixo <82701873+shirayukixo@users.noreply.github.com>
2021-04-20 21:51:47 +0800

    Add files via upload

---
+++ Exer3_1.java
@@ -0,0 +1,31 @@
+// Exercise 3 (Odd Sum and Even Sum)
+
+// Description of the Problem
+// Write an application that reads an integer and determines and prints whether
+// it is odd or even.
+
+import java.util.Scanner;
+
+public class Exer3_1 {
+
+	public static void main(String[] args) {
+		
+		// define Scanner
+		Scanner input = new Scanner(System.in);	
+		
+		// define variables
+		int num;
+		
+		// user input
+		System.out.print("Enter integer: ");
+		num = input.nextInt();
+		
+		// processing & print out
+		if (num % 2 == 0)
+			System.out.print("Number is even");
+		else
+			System.out.print("Number is odd");		
+
+	} // end main
+
+} // end class
